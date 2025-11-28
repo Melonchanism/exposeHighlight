bab3b2a518019cda461478bd1e46d594c3792dbc
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2025-11-27 19:43:16 -0800

    basically rewrite it to use userdefaults

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # exposeHighlight
 
-Small macOS mod that changes the color of the window hover border in Mission Control
-
-Inspired by [DockExposeHighlight](https://github.com/w0lfschild/DockExposeHighlight) but works on macOS 15+ for now at least
+Small macOS tweak that changes the color of the window hover border in Mission Control
+Inspired by [DockExposeHighlight](https://github.com/w0lfschild/DockExposeHighlight) but works on macOS 15, 26, and maybe some other older versions
 
 ### Installation (easy / automatic)
 [Disable SIP and set nvram](https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection)
@@ -13,21 +12,22 @@ Find some sort of injector (I will find some soon), and load it at login, and wh
 Depends on your form of installation, hope that you're a nerd already because I'm not the best at integrating everything
 
 ### Building
-Build the target, "exposeHighlight." exposeHighlight.dylib is now ready to be used with some sort of loader (I will find one soon).
+Build the target, "exposeHighlight" exposeHighlight.dylib is now ready to be used with some sort of loader (I will find one soon).
 
 ### Changing Color
-Until I add some way to configure it, `dockInjection.m` at line 11
+`defaults write com.apple.dock expose-highlight "#rrggbbaa"`
+or
+`defaults write com.apple.dock expose-highlight "#rrggbb"`
+
+The window highlight color will update immidiately, but the spaces bar outline will require `killall Dock`
 
 ### Is this safe???
-Maybe. I provide no recommendation whether you should use this, as it modifies system processes and requires reduced security settings.
-
+Define safe. By apple's standards, no.
 If something goes wrong, [restart in safe mode](https://support.apple.com/guide/mac-help/start-up-your-mac-in-safe-mode-mh21245/mac) and delete the files or loader
 
 ### Why?
-macOS is stupid in the way that the mission control window hover highlight doesn't care about your accent color or anything else; it is always system blue. Currently there is no way to easily set it to follow something, but I intend on doing that. Also people who use window borders might like the mission control hover to be the same color as their window borders.
+Because the macos mission control highlight doesn't care whatever color you choose, but always wants system blue.
 
 ### Other info
-This project makes extensive use of [Frida](https://frida.re) for function swapping.
+This project makes use of [Frida](https://frida.re) for function swapping.
 Also uses [ZKSwizzle](https://github.com/alexzielenski/ZKSwizzle) (MIT License)
-
-There is literally no way to debug crashes rn, I will eventually write a wrapper for frida and ZKSwizzle because I have other forks as well
--- exposeHighlight/dockInjection.m
+++ exposeHighlight/dockInjection.m
@@ -1,45 +1,99 @@
-#import <CoreGraphics/CoreGraphics.h>
-#import <Cocoa/Cocoa.h>
-#include "frida-gum.h"
 #include "ZKSwizzle.h"
+#include "frida-gum.h"
+#import <Cocoa/Cocoa.h>
+#import <CoreGraphics/CoreGraphics.h>
 
 CGColorRef CGColorCreateGenericRGB(CGFloat, CGFloat, CGFloat, CGFloat);
+CGColorRef CGColorCreateSRGB(CGFloat, CGFloat, CGFloat, CGFloat);
 
-static CGColorRef my_CGColorCreateGenericRGB(CGFloat red, CGFloat green, CGFloat blue, CGFloat alpha) {
-	// Replace color with custom one
-	NSLog(@"!Called Color Creation Function");
-	return CGColorCreateGenericRGB(96.5/255, 140.0/255, 77.0/255, 1.0);
+CGColorRef color;
+NSUserDefaults *defaults;
+
+CGColorRef colorFromHexString(NSString *hexString) {
+	CGColorRef fallback = CGColorCreateSRGB(1.0, 1.0, 1.0, 1.0);
+	if (![hexString isKindOfClass:[NSString class]]) {
+		return fallback;
+	}
+	if (hexString.length != 7 && hexString.length != 9) {
+		return fallback;
+	}
+	unsigned long long value = 0;
+	NSScanner *scanner = [NSScanner scannerWithString:hexString];
+	scanner.scanLocation = 1; // skip '#'
+	if (![scanner scanHexLongLong:&value]) {
+		return fallback;
+	}
+	CGFloat r, g, b, a;
+	if (hexString.length == 7) {
+		r = ((value & 0xFF0000) >> 16) / 255.0;
+		g = ((value & 0x00FF00) >> 8) / 255.0;
+		b = (value & 0x0000FF) / 255.0;
+		a = 1.0;
+	} else {
+		r = ((value & 0xFF000000) >> 24) / 255.0;
+		g = ((value & 0x00FF0000) >> 16) / 255.0;
+		b = ((value & 0x0000FF00) >> 8) / 255.0;
+		a = (value & 0x000000FF) / 255.0;
+	}
+	return CGColorCreateSRGB(r, g, b, a);
+}
+
+CGColorRef hook_CGColorCreateGenericRGB(CGFloat red, CGFloat green,
+                                        CGFloat blue, CGFloat alpha) {
+	NSLog(@"[colors]: %f, %f, %f", red, green, blue);
+
+	// Floating point accuracy in a nutshell
+	if (blue == 0.98431372549019602) {
+		color = colorFromHexString([defaults stringForKey:@"expose-highlight"]);
+		return color;
+	}
+	return CGColorCreateGenericRGB(red, green, blue, alpha);
 }
 
 void replace_color_function(void) {
-	GumInterceptor * interceptor;
-	
+	GumInterceptor *interceptor;
+
 	gum_init_embedded();
 	interceptor = gum_interceptor_obtain();
 	GError *error = nil;
-	GumModule *module = gum_module_load("/System/Library/CoreServices/Dock.app/Contents/MacOS/Dock", &error);
-	GumAddress funcAddr = gum_module_find_export_by_name (module, "CGColorCreateGenericRGB");
-	GumReplaceReturn result = gum_interceptor_replace(interceptor, (gpointer) funcAddr, my_CGColorCreateGenericRGB, NULL, NULL);
-	gum_interceptor_end_transaction (interceptor);
+	GumModule *module = gum_module_load(
+	    "/System/Library/CoreServices/Dock.app/Contents/MacOS/Dock", &error);
+	GumAddress funcAddr =
+	    gum_module_find_export_by_name(module, "CGColorCreateGenericRGB");
+	GumReplaceReturn _ =
+	    gum_interceptor_replace(interceptor, (gpointer)funcAddr,
+	                            hook_CGColorCreateGenericRGB, NULL, NULL);
+	gum_interceptor_end_transaction(interceptor);
 }
 
 __attribute__((constructor)) static void install(void) {
 	@autoreleasepool {
+		defaults = [NSUserDefaults standardUserDefaults];
 		replace_color_function();
 	}
 }
 
-//@interface WVMinimizedAndRecentsItemLayer : CALayer
-//- (struct CGRect)_frameForHighlight;
-//@end
-//
-//ZKSwizzleInterface(HookWVMinimizedAndRecentsItemLayer, WVMinimizedAndRecentsItemLayer, CALayer)
-//
-//@implementation HookWVMinimizedAndRecentsItemLayer
-//
-//- (struct CGRect)_frameForHighlight {
-//	NSLog(@"!Created border");
-//	return CGRectMake(0, 0, 0, 0);
+// MARK: Swizzle
+
+@interface WVMinimizedAndRecentsItemLayer : CALayer
+- (struct CGRect)_frameForHighlight;
+//- (CALayer *)_maskLayerForHighlight;
+//- (CALayer *)innerImageLayer;
+@end
+
+ZKSwizzleInterface(HookWVMinimizedAndRecentsItemLayer,
+                   WVMinimizedAndRecentsItemLayer, CALayer)
+    @implementation HookWVMinimizedAndRecentsItemLayer
+
+- (struct CGRect)_frameForHighlight {
+	CGRect rect = _orig(CGRect);
+	return CGRectMake(0, 0, 0, 0);
+}
+
+//- (CALayer *)innerImageLayer {
+//	CALayer *layer = _orig(CALayer *);
+//	NSLog(@"[d]: [WVMinimizedAndRecentsItemLayer innerImageLayer]");
+//	return layer;
 //}
-//
-//@end
+
+@end
